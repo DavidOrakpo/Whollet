bb8fe7b1b1fecc05d58d4ec06d428a9c5583f86f
David Orakpo <davidorakpo65@gmail.com>
2022-02-06 14:20:02 +0100

    Added Sqlite DB

---
+++ Whollet/Whollet/Model/Helpers/Database.cs
@@ -0,0 +1,53 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Whollet.Model.Helpers
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            //Establishing the conection
+            _database = new SQLiteAsyncConnection(dbPath);
+            // write each statement for the amount of tables you wish to create
+            _database.CreateTableAsync<User>().Wait();
+            _database.CreateTableAsync<Address>().Wait();
+        }
+
+        // Show the registers
+        public Task<List<T>> GetTableAsync<T>(T model) where T : new()
+        {
+            var temp = _database.Table<T>().ToListAsync();         
+            return temp;
+        }
+
+        // Save registers
+        public Task<int> SaveAsync<T>(T model) where T : new()
+        {
+            return _database.InsertAsync(model);
+        }
+
+        public Task<T> GetWithChidsAsync<T>(int objectID) where T : new()
+        {
+            return _database.GetAsync<T>(objectID);
+        }
+
+        // Delete registers
+        public Task<int> DeleteAsync<T>(T model) where T : new()
+        {
+            return _database.DeleteAsync(model);
+        }
+
+        // Save registers
+        public Task<int> UpdateAsync<T>(T model) where T: new()
+        {
+            return _database.UpdateAsync(model);
+        }
+    }
+}
---
+++ Whollet/Whollet/Model/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using SQLitePCL;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Whollet.Model
+{   [Table("Users")]
+    public class User
+    {
+        [AutoIncrement, PrimaryKey]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password 
+        {
+            get
+            {
+                var _unlock1 = SecureStorage.GetAsync(FirstName + LastName + "key").Result;
+                var _unlock2 = SecureStorage.GetAsync(_unlock1).Result;
+                return _unlock2;
+            }
+            set
+            {
+                var _passKey = RandomString(20);
+                SecureStorage.SetAsync(FirstName + LastName + "key", _passKey);
+                SecureStorage.SetAsync(_passKey, value);
+               // _password = value;
+            }
+        }
+        public int Pincode { get; set; }
+        public string NationalID { get; set; } = String.Empty;
+        public string Passport { get; set; } = String.Empty;
+        public string Drivers_license { get; set; } = String.Empty; 
+
+        [OneToOne(CascadeOperations = CascadeOperation.All)]
+        public Address address { get; set; }
+       
+
+        public string RandomString(int size, bool lowerCase = false)
+        {
+            var builder = new StringBuilder(size);
+            var _random = new Random();
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.  
+
+            // char is a single Unicode character  
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length=26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)_random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+    }
+    
+
+}
