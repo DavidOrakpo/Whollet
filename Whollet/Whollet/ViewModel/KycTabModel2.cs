86f3c432f7d38c9862678aa2a9f4da902117b0cd
David Orakpo Orakpo <davidorakpo65@gmail.com>
2022-02-16 08:40:12 +0100

    Succesfully able to figure out how to Refresh the UI. Tab page working smoothly and concisely.

--- Whollet/Whollet/ViewModel/KycTabModel2.cs
+++ Whollet/Whollet/ViewModel/KycTabModel2.cs
@@ -7,91 +7,51 @@ using Xamarin.Forms;
 using Whollet.Views.KYC;
 using Whollet.Views.FirstTimeInApp;
 using Whollet.ViewModel.EmptyStateModels;
+using System.Threading.Tasks;
 
 namespace Whollet.ViewModel
 {
     public class KycTabModel2 : BaseViewModel
     {
         private Dictionary<string, ContentView> ContentViewDictionary = new Dictionary<string, ContentView>();
-        private KycEmptyPage _tabbedPage;
-        //EmptyStateViewModel emptyStateViewModel = new EmptyStateViewModel();
         ContentView currentview;
 
         public KycTabModel2()
         {
-
-            
-           // _tabbedPage.BindingContext = this;
             ContentViewDictionary.Add("First_View", new EmptyStateView());
             ContentViewDictionary.Add("Second_View", new EmptyStatePending());
             ContentViewDictionary.Add("Third_View", new EmptyStateFinished());
-       
-            // var currentview = _tabbedPage.Middle_tab.Content as ContentView;
-           // emptyStateViewModel.CallNextViewEvent += EmptyStateViewModel_CallNextViewEvent;
-            currentview = ContentViewDictionary["First_View"];
-          //  _tabbedPage.Middle_tab.Content = currentview;
+            Currentview = ContentViewDictionary["First_View"];
         }
 
         public KycTabModel2(KycEmptyPage tabbedpage)
         {
-
-            _tabbedPage = tabbedpage;
-            //_tabbedPage.BindingContext = this;
             ContentViewDictionary.Add("First_View", new EmptyStateView());
             ContentViewDictionary.Add("Second_View", new EmptyStatePending());
             ContentViewDictionary.Add("Third_View", new EmptyStateFinished());
-            // var currentview = _tabbedPage.Middle_tab.Content as ContentView;
-
-            currentview = ContentViewDictionary["First_View"];
-            //currentview.BindingContext = this;
-            _tabbedPage.Middle_tab.Content = currentview;
-            _tabbedPage.Middle_tab.BindingContext = this;
-
-           // emptyStateViewModel.CallNextViewEvent += EmptyStateViewModel_CallNextViewEvent;
+            Currentview = ContentViewDictionary["First_View"];
         }
 
-        //private void EmptyStateViewModel_CallNextViewEvent(object sender, EventArgs e)
-        //{
-        //    // ChangeViewCommand.Execute(null);
-        //    Next();
-        //}
 
         public Command ChangeViewCommand => new Command(() =>
         {
-            if (currentview == ContentViewDictionary["First_View"])
-            {
-                currentview = ContentViewDictionary["Second_View"];
-                currentview.BindingContext = this;
-                _tabbedPage.Middle_tab.Content = currentview;
-                
-                //_tabbedPage.KycTabView.SelectedIndex = 1;
-               // OnPropertyChanged(nameof(_tabbedPage.Middle_tab.Content));
+            if (Currentview == ContentViewDictionary["First_View"])
+            {              
+                    Currentview = ContentViewDictionary["Second_View"];
+                    Currentview.BindingContext = this;
+                    OnPropertyChanged(nameof(Currentview));
             }
-            else if (currentview == ContentViewDictionary["Second_View"])
+            else if (Currentview == ContentViewDictionary["Second_View"])
             {
-                // _tabbedPage.Middle_tab.Content.BindingContext = null;
-                //_tabbedPage.Middle_tab.Content.BindingContext = this;
-
-                
-                    currentview = ContentViewDictionary["Third_View"];
-                    currentview.BindingContext = this;
-                    _tabbedPage.Middle_tab.Content = currentview;
-                    ///Code to update view here
-               
-                //OnPropertyChanged(nameof(_tabbedPage.Middle_tab.Content));
+                    Currentview = ContentViewDictionary["Third_View"];
+                    Currentview.BindingContext = this;
             }
-            else if (currentview == ContentViewDictionary["Third_View"])
+            else if (Currentview == ContentViewDictionary["Third_View"])
             {
-                //_tabbedPage.Middle_tab.Content.BindingContext = null;
-                _tabbedPage.Middle_tab.Content.BindingContext = this;
+                Currentview.BindingContext = this;
                 Application.Current.MainPage.DisplayAlert("Testing", "It works", "Ok");
             }
-
-
         });
-
-        
-
         public ContentView Currentview
         {
             get
@@ -102,26 +62,8 @@ namespace Whollet.ViewModel
             set
             {
                 currentview = value;
-               // OnPropertyChanged();
+                OnPropertyChanged();
             }
         }
-
-        private void Next()
-        {
-            if (Currentview == ContentViewDictionary["First_View"])
-            {
-                Currentview = ContentViewDictionary["Second_View"];
-            }
-            else if (Currentview == ContentViewDictionary["Second_View"])
-            {
-                Currentview = ContentViewDictionary["Third_View"];
-            }
-        }
-
-        //private void EventFired()
-        //{
-            
-        //    emptyStateViewModel.CallNextViewEvent += EmptyStateViewModel_CallNextViewEvent;
-        //}
     }
 }
